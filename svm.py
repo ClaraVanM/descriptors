bcd8a898b75c1ec01351f85af75fdb441ab2e9d6
ClaraVanM <clara.van.meegen@gmail.com>
2024-04-26 18:03:37 +0200

    done descriptor calculations not3.2.1

--- svm.py
+++ svm.py
@@ -1,3 +1,9 @@
 import pandas as pd
-df = pd.read_csv('descriptors_not3.2.1.csv')
-print(df.shape)
+
+pockets = "/home/r0934354/Documents/overview_not3.2.1.csv"
+total = "/home/r0934354/Documents/overview_not3.2.1_faults.csv"
+
+df_pockets = pd.read_csv(pockets, index_col=0)
+df_total = pd.read_csv(total, index_col=0)
+df = pd.merge(df_pockets, df_total, on='id', how='right')
+print(df)
