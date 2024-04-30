529d377323833c9d81024f49b065adfc96de3e09
ClaraVanM <clara.van.meegen@gmail.com>
2024-04-30 16:18:46 +0200

    svm code

--- svm.py
+++ svm.py
@@ -1,34 +1,44 @@
 import pandas as pd
-from sklearn import  svm
-
-"""df1 = pd.read_csv("descriptors_not3.2.1.csv", index_col=0)
-df1 = df1[df1["name"] != "-2"]"""
-
-from sklearn.datasets import load_breast_cancer
 from sklearn.svm import SVC
-from skopt import gp_minimize
-from skopt.space import Real, Categorical
-from skopt.utils import use_named_args
-from sklearn.model_selection import cross_val_score
+from skopt.space import Real, Categorical, Integer
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from skopt import BayesSearchCV
 
-data = load_breast_cancer()
-X,y = data.data, data.target
+
+########################################################################################################################
+df1 = pd.read_csv("descriptors_not3.2.1.csv", index_col=0)
+df1 = df1[df1["name"] != "-2"]
+df1["group"] = 0
+df2 = pd.read_csv("descriptors_ec3.2.1.csv", index_col=0)
+df2 = df2[df2["name"] != "-2"]
+df2['group'] = 1
+df = pd.concat([df1, df2])
+
+########################################################################################################################
+
+X = df.drop(columns=["group", 'name'])
+X.to_csv('X.csv')
+##sometimes narrowness and corresponding buriedness composition is NaN?
+#for now put NaN to 0
+X = X.fillna(0)
+y = df["group"]
+scaler = StandardScaler()
+X = scaler.fit_transform(X)
+
+#stratified split because response is unbalances, now the training set has overall an equal amount of both groups as the original dataset
+X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.75, random_state=0, stratify=y)
 
 #define parameter space
-param_space = [Real(0.1, 100, "log-uniform", name = "c"),
-               Real(0.01, 10, "log-uniform", name="gamma"),
-               Categorical(["rbf", "linear", "poly"], name="kernel")]
+opt = BayesSearchCV(SVC(), {
+    'C':Real(0.1, 100, "log-uniform"),
+    'gamma':Real(0.01, 10, "log-uniform"),
+    'degree': Integer(1,8),
+    'kernel': Categorical(["rbf", "linear", "poly"])},
+           n_iter=32,
+            random_state=0)
 
-#define objective function
-@use_named_args(param_space)
-def objective(**params):
-    clf = SVC(**params)
-    accuracy = cross_val_score(clf, X,y, cv=5,n_jobs=1).mean()
-    return -accuracy
+_ = opt.fit(X_train, y_train)
+print(opt.score(X_test, y_test))
 
-res_gp = gp_minimize(objective, param_space, n_calls=20, random_state=0)
-
-best_params = dict(zip(['C', 'gamma', 'kernel'], res_gp.x))
-
-print('Best parameters:', best_params)
-print('Best accuracy', -res_gp.fun)
+### accuracy = 0.8677
